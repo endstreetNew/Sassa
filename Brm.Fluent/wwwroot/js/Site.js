59bed28b94268c407d9e9d8ddea1b0bf7210a673
Johan Smit <johan.smit@faranani.com>
2024-08-29 11:23:49 +0200

    FluentToast

--- Brm.Fluent/wwwroot/js/Site.js
+++ Brm.Fluent/wwwroot/js/Site.js
@@ -25,3 +25,56 @@ function triggerFileDownload(fileName, url)
     anchorElement.remove();
 }
 
+//const prodTheme = createProdTheme({
+//    palette: {
+//        themePrimary: '#0078d4',
+//        themeLighterAlt: '#eff6fc',
+//        themeLighter: '#deecf9',
+//        themeLight: '#c7e0f4',
+//        themeTertiary: '#71afe5',
+//        themeSecondary: '#2b88d8',
+//        themeDarkAlt: '#106ebe',
+//        themeDark: '#005a9e',
+//        themeDarker: '#004578',
+//        neutralLighterAlt: '#faf9f8',
+//        neutralLighter: '#f3f2f1',
+//        neutralLight: '#edebe9',
+//        neutralQuaternaryAlt: '#e1dfdd',
+//        neutralQuaternary: '#d0d0d0',
+//        neutralTertiaryAlt: '#c8c6c4',
+//        neutralTertiary: '#a19f9d',
+//        neutralSecondary: '#605e5c',
+//        neutralPrimaryAlt: '#3b3a39',
+//        neutralPrimary: '#323130',
+//        neutralDark: '#201f1e',
+//        black: '#000000',
+//        white: '#ffffff',
+//    }
+//});
+
+//const qaTheme = createQaTheme({
+//    palette: {
+//        themePrimary: '#0d9e2a',
+//        themeLighterAlt: '#f2fbf4',
+//        themeLighter: '#ccefd3',
+//        themeLight: '#a4e2b0',
+//        themeTertiary: '#58c56e',
+//        themeSecondary: '#20aa3c',
+//        themeDarkAlt: '#0b8e26',
+//        themeDark: '#0a7820',
+//        themeDarker: '#075917',
+//        neutralLighterAlt: '#faf9f8',
+//        neutralLighter: '#f3f2f1',
+//        neutralLight: '#edebe9',
+//        neutralQuaternaryAlt: '#e1dfdd',
+//        neutralQuaternary: '#d0d0d0',
+//        neutralTertiaryAlt: '#c8c6c4',
+//        neutralTertiary: '#a19f9d',
+//        neutralSecondary: '#605e5c',
+//        neutralPrimaryAlt: '#3b3a39',
+//        neutralPrimary: '#323130',
+//        neutralDark: '#201f1e',
+//        black: '#000000',
+//        white: '#ffffff',
+//    }
+//});
